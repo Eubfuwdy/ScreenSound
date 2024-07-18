ded102d142bd290300884e3841af442aa2a3b0d5
marco <marcos.rogerio.oliveira23@gmail.com>
2024-07-18 11:02:46 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound-04/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ScreenSound_04.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicaFavoritas { get; }
+
+    public MusicasPreferidas(string nome) 
+    {
+        Nome = nome;
+        ListaDeMusicaFavoritas = new List<Musica>();
+
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicaFavoritas.Add(musica);
+    }
+
+    public void ExibirMuiscasFavoritas()
+    {
+        Console.WriteLine($"Essas são as musicas favoritas -> {Nome}");
+        foreach (var musica in ListaDeMusicaFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+        }
+        Console.WriteLine();
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = ListaDeMusicaFavoritas
+        });
+        string nomeDoArquivo = $"musicas -favoritas-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"O arquivo Json foi criado com sucesso! { Path.GetFullPath(nomeDoArquivo)} ");
+    }
+}
---
+++ ScreenSound-04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using ScreenSound_04.Modelos;
+using ScreenSound_04.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync
+        ("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        LinqFilter.FiltrarMusicasEmCSharp(musicas);
+
+        //musicas[0].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+
+        //var musicasPreferidasDoMarcos = new MusicasPreferidas("Marcos");
+        //musicasPreferidasDoMarcos.AdicionarMusicasFavoritas(musicas[1]);
+        //musicasPreferidasDoMarcos.AdicionarMusicasFavoritas(musicas[2]);
+        //musicasPreferidasDoMarcos.AdicionarMusicasFavoritas(musicas[3]);
+        //musicasPreferidasDoMarcos.AdicionarMusicasFavoritas(musicas[4]);
+        //musicasPreferidasDoMarcos.AdicionarMusicasFavoritas(musicas[5]);
+
+        //musicasPreferidasDoMarcos.ExibirMuiscasFavoritas();
+
+        //musicasPreferidasDoMarcos.GerarArquivoJson();
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
